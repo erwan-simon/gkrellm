9ade44c7b8d47307cd790f81304c1cb2306f0dff
erwan <erwan.simon@epitech.eu>
2017-01-21 14:46:46 +0100

    core

--- Core.cpp
+++ Core.cpp
@@ -5,13 +5,17 @@
 // Login   <erwan.simon@epitech.eu>
 // 
 // Started on  Sat Jan 21 14:06:57 2017 erwan
-// Last update Sat Jan 21 14:42:00 2017 erwan
+// Last update Sat Jan 21 14:46:37 2017 erwan
 //
 
 #include <string>
 #include "Core.hpp"
 
 Core::Core()
+{
+}
+
+Core::~Core()
 {
 }
 
@@ -26,66 +30,16 @@ Core&	Core::operator=(Core const &other)
   return (*this);
 }
 
-int	Core::getCoreNb() const
-{
-  return (this->_coreNb);
-}
+int	Core::getCoreNb() const				{return (this->_coreNb);}
+float	*Core::getCorePercent() const			{return (this->_corePercent);}
+float	*Core::getRam() const				{return (this->_ram);}
+float	*Core::getSwap() const				{return (this->_swap);}
+int	*Core::getTasksNb() const			{return (this->_tasksNb);}
+float	*Core::getLoadAvg() const			{return (this->_loadAvg);}
 
-float	*Core::getCorePercent() const
-{
-  return (this->_corePercent);
-}
-
-float	*Core::getRam() const
-{
-  return (this->_ram);
-}
-
-float	*Core::getSwap() const
-{
-  return (this->_swap);
-}
-
-int	*Core::getTasksNb() const
-{
-  return (this->_tasksNb);
-}
-
-float	*Core::getLoadAvg() const
-{
-  return (this->_loadAvg);
-}
-
-void	Core::setCoreNb(int coreNb)
-{
-  this->_coreNb = coreNb;
-}
-
-void	Core::setCorePercent(float *corePercent)
-{
-  this->_corePercent = corePercent;
-}
-
-void	Core::setRam(float *ram)
-{
-  this->_ram = ram;
-}
-
-void	Core::setSwap(float *swap)
-{
-  this->_swap = swap;
-}
-
-void	Core::setTasksNb(int *tasksNb)
-{
-  this->_tasksNb = tasksNb;
-}
-
-void	Core::setLoadAvg(float *loadAvg)
-{
-  this->_loadAvg = loadAvg;
-}
-
-Core::~Core()
-{
-}
+void	Core::setCoreNb(int coreNb)			{this->_coreNb = coreNb;}
+void	Core::setCorePercent(float *corePercent)	{this->_corePercent = corePercent;}
+void	Core::setRam(float *ram)			{this->_ram = ram;}
+void	Core::setSwap(float *swap)			{this->_swap = swap;}
+void	Core::setTasksNb(int *tasksNb)			{this->_tasksNb = tasksNb;}
+void	Core::setLoadAvg(float *loadAvg)		{this->_loadAvg = loadAvg;}
