b3c9db3a51feba69ec6c8bd095a937f18ad1dcfc
antoine <antoine.roche@epitech.eu>
2017-01-21 20:26:16 +0100

    main

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // Login   <antoine.roche@epitech.eu>
 // 
 // Started on  Sat Jan 21 19:53:10 2017 antoine
-// Last update Sat Jan 21 20:09:42 2017 antoine
+// Last update Sat Jan 21 20:24:42 2017 antoine
 //
 
 #include <iostream>
@@ -15,7 +15,9 @@
 
 void    help_mode()
 {
-  std::cout << "help" << std::endl;
+  std::cout << "MyGKrellM 1.0.0 - (C) 2016 Epitech Rush 3/3 \n" << std::endl;
+  std::cout << "-g		Display on the graphical interface"<< std::endl;
+  std::cout << "-h --help	Print this help screen \n" << std::endl;
 }
 
 int	main(int ac, char ** av, char **env)
